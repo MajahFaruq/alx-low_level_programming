06e70e28d6c6b8cafab91d4ceaf26d716a485e13
Majah Faruq <holawale46@gmail.com>
2022-10-13 12:15:08 -0700

    git

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 /*put instead of print*/
 
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	retur (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
